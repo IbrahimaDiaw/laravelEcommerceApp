0bab6917b65b22fef8ff22611ca23b0e19b98d62
Ibrahima DIAW <ibrahimadiaw1997@gmail.com>
2022-02-20 18:05:40 +0000

    Laravel Ecommerce app built at 2020

--- routes/api.php
+++ routes/api.php
@@ -14,26 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('login', 'Auth\User\LoginController@login');
-Route::post('register', 'Auth\User\LoginController@userRegister');
-
-Route::post('auth/login', 'Auth\Vendor\LoginController@login');
-Route::post('auth/register', 'Auth\Vendor\LoginController@vendorRegister');
-
-Route::post('auth/admin/login', 'Auth\Admin\LoginController@login');
-Route::post('auth/admin/register', 'Auth\Admin\LoginController@adminRegister');
-
 Route::group(['middleware' => 'jwt.auth'], function () {
-    Route::get('admin', 'Auth\Admin\LoginController@getAuthAdmin');
-});
-
-
-
-Route::group(['middleware' => 'jwt.auth'], function () {
-    Route::get('user', 'Auth\User\LoginController@getAuthUser');
-});
-
-
-Route::group(['middleware' => 'jwt.auth'], function () {
-    Route::get('vendor', 'Auth\Vendor\LoginController@getAuthVendor');
+    return 'Hello World';
 });
